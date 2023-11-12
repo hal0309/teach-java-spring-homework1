4e7a6a74282576ea17f2754089d62f1d2e7bbfd3
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2023-11-12 15:25:48 +0900

    nakano create homework

---
+++ src/main/java/com/watanabe/homework1/model/AnimalModel.java
@@ -0,0 +1,15 @@
+package com.watanabe.homework1.model;
+
+public class AnimalModel {
+    public String name;
+    public String type;
+    public AnimalModel(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+
+    public void hello() {
+        System.out.println("Hello, I am Animal");
+    }
+}
